e011b515944bc0a960c90bf9fe4911669c815adc
developiaa <developiaa@gmail.com>
2023-01-03 23:56:14 +0900

    first commit

---
+++ src/main/java/study/developia/oauth/SpringOauthStudyApplication.java
@@ -0,0 +1,13 @@
+package study.developia.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauthStudyApplication.class, args);
+    }
+
+}
